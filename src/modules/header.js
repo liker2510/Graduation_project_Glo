8ac0b224ee0973160386e8ceb78dc35f54b2fac3
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-19 21:27:01 +0500

    плавный скорлл

--- src/modules/header.js
+++ src/modules/header.js
@@ -6,10 +6,21 @@ const one = () => {
     const arrow = document.querySelector('.header-contacts__arrow');
     const headerMenu = document.querySelector('.menu>.menu__icon');
     const popupMenu = document.querySelector('.popup');
-    console.log(headerMenu);
-    console.log(popupMenu);
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
 
 
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+    
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+        
     headerMenu.addEventListener('click', () => {
         const screenWidth = window.screen.width;
         if (screenWidth >= '767') {
@@ -19,37 +30,23 @@ const one = () => {
                     return 1 - Math.sin(Math.acos(timeFraction));
                 },
                 draw(progress) {
-                    // popupMenu.style.cssText = "width: 0%;";
-                    popupMenu.style.width = progress * 44 + '%';
-                    console.log(progress);
+                    popupMenu.style.right = progress * 46 + '%';
+                    popupMenu.style.float = "right";
                 }
             });
         }
     })
 
     popupMenu.addEventListener('click', (e) => {
-        if (e.target.classList.contains('close-menu')) {
-            const screenWidth = window.screen.width;
-            if (screenWidth >= '767') {
-                animate({
-                    duration: 1200,
-                    timing(timeFraction) {
-                        return 1 - Math.sin(Math.acos(timeFraction));
-                    },
-                    draw(progress) {
-                        // popupMenu.style.cssText = "width: 0%;";
-                        popupMenu.style.width = progress * 100 + '%';
-                        console.log(progress);
-                    }
-                });
-            }
+        if (e.target.classList.contains('menu-link') || e.target.classList.contains('close-menu')) {
+            popupMenu.style.cssText = "right: 0%;";
         }
     })
 
 
     headerContactsArrow.addEventListener('click', () => {
-        arrow.classList.toggle('header-contacts__arrow__active');
-        scrollPhone.classList.toggle('header-contacts__phone-number-accord');
+        arrow.classList.toggle('header-accord-arrow-active');
+        scrollPhone.classList.toggle('header-accord-active');
     })
 
 
