53ddbc9c0d4b145a95804ece9d6626306e3902d0
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-20 14:21:07 +0500

    Подсказка

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,9 +1,44 @@
-
+import { animate } from "./helpers";
 const popup = () => {
     const popupRepairTypes = document.querySelector('.popup-repair-types');
     const menuLink = document.querySelectorAll('.open_popup');
     const popupPrivacy = document.querySelector('.popup-privacy');
     const linkPrivacy = document.querySelectorAll('.link-privacy');
+    const formula = document.getElementById('formula');
+    const formulaItemIcon = formula.querySelectorAll('.formula-item__icon');
+
+    
+    formulaItemIcon.forEach((index) => {
+        index.addEventListener('mouseover', () => {
+            animate({
+                duration: 300,
+                timing(timeFraction) {
+                    return 1 - Math.sin(Math.acos(timeFraction));
+                },
+                draw(progress) {
+                    index.firstElementChild.style.opacity = progress;
+                }
+            });
+            if (index.innerText === '01') {
+                index.firstElementChild.style = "visibility: inherit; ";
+            } else if (index.innerText === '02') {
+                index.firstElementChild.style = "visibility: inherit;";
+            } else if (index.innerText === '03') {
+                index.firstElementChild.style = "visibility: inherit;";
+            } else if (index.innerText === '04') {
+                index.firstElementChild.style = "visibility: inherit;";
+            } else if (index.innerText === '05') {
+                index.firstElementChild.style = "visibility: inherit;";
+            } else if (index.innerText === '06') {
+                index.firstElementChild.style = "visibility: inherit;";
+            }
+        })
+
+        index.addEventListener('mouseout', () => {
+            index.firstElementChild.style = "";
+        })
+    })
+    
     
     linkPrivacy.forEach((index) => {
         index.addEventListener('click', () => {
