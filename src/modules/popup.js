4543c6bf6531fbb0d8d4edb92f6e1e3fb83fa275
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-20 15:36:53 +0500

    Консультация

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -6,6 +6,18 @@ const popup = () => {
     const linkPrivacy = document.querySelectorAll('.link-privacy');
     const formula = document.getElementById('formula');
     const formulaItemIcon = formula.querySelectorAll('.formula-item__icon');
+    const buttons = document.querySelectorAll('.button_wide');
+    const popupConsultation = document.querySelector('.popup-consultation');
+
+    buttons.forEach((index) => {
+        index.addEventListener('click', () => {
+            popupConsultation.style = 'visibility: inherit;';
+        })
+    })
+
+    // popupConsultation.addEventListener(() => {
+
+    // }) 
 
     
     formulaItemIcon.forEach((index) => {
@@ -50,15 +62,18 @@ const popup = () => {
     menuLink.forEach((index) => {
         index.addEventListener('click',() => {
             popupRepairTypes.style = 'visibility: inherit;';
-            console.log(close);
         })
     })
 
     document.body.addEventListener('click', (e) => {
-        if (e.target.classList.contains('mobile-hide')) {
-            popupRepairTypes.style = "visibility: hidden;";
-            popupPrivacy.style = "visibility: hidden;";
-        }
+        if (e.target.classList.contains('close')) {
+            popupRepairTypes.style = "";
+            popupPrivacy.style = "";
+            popupConsultation.style = '';
+        } 
+        // else if (e.target.classList.contains('close-consultation')) {
+            
+        // }
     })
 
 }
