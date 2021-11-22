c7bb382a146611699eeaa64cfc841832a1bd13cb
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-22 21:51:12 +0500

    Меню

--- src/modules/header.js
+++ src/modules/header.js
@@ -6,6 +6,7 @@ const one = () => {
     const arrow = document.querySelector('.header-contacts__arrow');
     const headerMenu = document.querySelector('.menu>.menu__icon');
     const popupMenu = document.querySelector('.popup');
+    const openPopupMenu = document.querySelector('.popup-dialog-menu');
     const smoothLinks = document.querySelectorAll('a[href^="#"]');
 
 
@@ -22,27 +23,14 @@ const one = () => {
     };
         
     headerMenu.addEventListener('click', () => {
-        const screenWidth = window.screen.width;
-        if (screenWidth >= '767') {
-            animate({
-                duration: 1200,
-                timing(timeFraction) {
-                    return 1 - Math.sin(Math.acos(timeFraction));
-                },
-                draw(progress) {
-                    popupMenu.style.right = progress * 46 + '%';
-                    popupMenu.style.float = "right";
-                }
-            });
-        }
+        openPopupMenu.classList.add('showHide-menu');
     })
 
     popupMenu.addEventListener('click', (e) => {
         if (e.target.classList.contains('menu-link') || e.target.classList.contains('close-menu')) {
-            popupMenu.style.cssText = "right: 0%;";
+            openPopupMenu.classList.remove('showHide-menu');
         }
     })
-
 
     headerContactsArrow.addEventListener('click', () => {
         arrow.classList.toggle('header-accord-arrow-active');
